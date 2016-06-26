ea3941414cef11095d619db2f5e4c40fd35907fe
Steve Domin <steve@stevedomin.com>
2016-06-26 13:28:13 +0100

    Fix tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ExampleApplication.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Phoenix Swoosh Example Application"
   end
 end
